9911f26c7fcb0f37677f9409bbb90acd66567909
Ayush Garg <142652768+AyushGarg76@users.noreply.github.com>
2026-06-04 00:47:51 +0530

    Time: 12 ms (80.70%) | Memory: 13.5 MB (32.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,22 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        left = 0
-        right = len(s) - 1
-
-        while left < right:
-
-            while left < right and not s[left].isalnum():
-                left += 1
-
-            while left < right and not s[right].isalnum():
-                right -= 1
-
-            if s[left].lower() != s[right].lower():
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
-
+        a = s.strip().lower() 
+        cleaned = ''.join([char for char in a if char.isalnum()]) 
+        if cleaned == cleaned[::-1]: 
+            return True 
+        return False
