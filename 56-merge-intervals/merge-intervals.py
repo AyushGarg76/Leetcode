d86f17f041607b8efbe9affc64e14e51d5974dfd
Ayush Garg <142652768+AyushGarg76@users.noreply.github.com>
2026-06-19 00:15:19 +0530

    Time: 11 ms (57.79%) | Memory: 17.2 MB (8.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,15 +6,23 @@ class Solution(object):
         """
         intervals.sort(key=lambda x: x[0])
 
-        merged = [intervals[0]]
+        result = []
 
-        for start, end in intervals[1:]:
-            last_end = merged[-1][1]
+        start1 = intervals[0][0]
+        end1 = intervals[0][1]
 
-            if start <= last_end:
-                merged[-1][1] = max(last_end, end)
-            else:
-                merged.append([start, end])
+        for i in range(1, len(intervals)):
+            start2 = intervals[i][0]
+            end2 = intervals[i][1]
 
-        return merged
-        
+            if start2 <= end1:          # overlap
+                end1 = max(end1, end2)
+            else:                       # no overlap
+                result.append([start1, end1])
+
+                start1 = start2
+                end1 = end2
+
+        result.append([start1, end1])
+
+        return result
